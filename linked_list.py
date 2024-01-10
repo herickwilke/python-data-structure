b4f589efa5d9a7fd78f4a9a2d6566fd69620d5a2
Herick Wilke <herickwilke@Hericks-MacBook-Pro.local>
2024-01-10 09:18:21 -0300

    Added Insert method to my LL

--- linked_list.py
+++ linked_list.py
@@ -96,17 +96,31 @@ class LinkedList:
             temp.value = value
             return True
         return False
+    
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
+            return False
+        if index == 0:
+            return self.prepend(value)
+        if index == self.length:
+            return self.append(value)
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1
+        return True
 
 
 my_linked_list = LinkedList(0)
-my_linked_list.append(1)
 my_linked_list.append(2)
-my_linked_list.append(3)
 
-my_linked_list.set_value(1, 224)
+my_linked_list.insert(1, 1)
 
 my_linked_list.print_list()
 
+
+# my_linked_list.set_value(1, 224)
 
 # print(my_linked_list.get(2))
 
